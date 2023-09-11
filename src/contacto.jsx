566dfe1d4f808068c69ccf310b4e375ec7d8eb92
Vilt12 <112113374+Vilt12@users.noreply.github.com>
2023-09-11 00:53:44 -0300

    Diseños responsive

--- src/contacto.jsx
+++ src/contacto.jsx
@@ -10,10 +10,10 @@ function Contacto() {
   <Navbar></Navbar>
   <div className="box-contacto">
 <div className="box-icons">
-   <Link to=""><img   width={"200px"} src="https://cdn-icons-png.flaticon.com/128/733/733547.png" alt="" /></Link>
-   <Link to=""><img   width={"200px"} src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png" alt="" /></Link>
-   <Link to=""><img   width={"200px"}src="https://cdn-icons-png.flaticon.com/128/2111/2111728.png" alt="" /></Link>
-   <Link to=""> <img  width={"200px"} src="https://cdn-icons-png.flaticon.com/128/732/732200.png" alt="" /></Link>
+   <Link to=""><img   src="https://cdn-icons-png.flaticon.com/128/733/733547.png" alt="" /></Link>
+   <Link to=""><img   src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png" alt="" /></Link>
+   <Link to=""><img  src="https://cdn-icons-png.flaticon.com/128/2111/2111728.png" alt="" /></Link>
+   <Link to=""> <img  src="https://cdn-icons-png.flaticon.com/128/732/732200.png" alt="" /></Link>
 
 </div>
   
--- src/section-2.jsx
+++ src/section-2.jsx
@@ -8,7 +8,7 @@ function Section2() {
         <div>
       <div className="section__2-box">
         <div className="section__2-box-img">
-            <img  src="https://images.unsplash.com/photo-1559925393-8be0ec4767c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FmZSUyMHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt="" height={"300px"} width={"600px"} />
+            <img  src="https://images.unsplash.com/photo-1559925393-8be0ec4767c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FmZSUyMHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""  />
         </div>
         <div className="section__2-box-titulo">
             <h4>Nuestro local</h4>
@@ -28,12 +28,12 @@ function Section2() {
             </div>
         </div>
         <div className="section__2-box-img">
-            <img src="https://images.wallpaperscraft.com/image/single/glass_coffee_light_115603_1920x1080.jpg" alt="" height={"300px"} width={"600px"} />
+            <img src="https://images.wallpaperscraft.com/image/single/glass_coffee_light_115603_1920x1080.jpg" alt=""  />
         </div>
       </div>
       <div className="section__2-box">
         <div className="section__2-box-img">
-            <img  src="https://static.vecteezy.com/system/resources/previews/015/194/832/non_2x/working-together-two-young-cafe-employees-indoors-conception-of-business-and-service-photo.jpg" alt="" height={"300px"} width={"600px"} />
+            <img  src="https://static.vecteezy.com/system/resources/previews/015/194/832/non_2x/working-together-two-young-cafe-employees-indoors-conception-of-business-and-service-photo.jpg" alt=""  />
         </div>
         <div className="section__2-box-titulo">
             <h4>¿Quienes somos?</h4>
