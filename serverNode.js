60249effe16bda1b5b59552ee725bdfcda2c33e1
Vilt12 <112113374+Vilt12@users.noreply.github.com>
2023-09-30 16:10:01 -0300

    Prueba de back

--- serverNode.js
+++ serverNode.js
@@ -34,4 +34,68 @@ app.listen(port,function(){
 })
 
 
+const cors = require("cors");
+//
+
+const mercadopago = require("mercadopago");
+
+
+//REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
+//Uso de token para autenticar las solicitudes a la Api de MP
+mercadopago.configure({
+	access_token: "APP_USR-2050192253529060-082910-7aca82a1134f8547b624bd3dfdcbf6d6-1463144691"
+});
+//
+
+//Configuracion para manejar solicitudes HTTP entrantes uwu(Json,URL codificados,CORS) este ultimo nos permite hacer solicitudes desde diferentes origenes
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("../../client/html-js"));
+app.use(cors());
+//
+
+//Ruta Raiz
+app.get("/", function(req,res){
+    res.send("Si funciona uwu")
+})
+//
+
+
+//Esta ruta ("/create_preference") maneja una solicitud POST que utiliza para crear una preferencia de pago en mercado pago
+//Esta solicitud debe contener un objeto JSON con los elementos "items" y "totalAmount"
+app.post("/create_preference", (req, res) => {
+	const { items, totalAmount } = req.body;
+  
+	let preference = {
+	  items: items,
+	  // ... (otras configuraciones de preferencia)
+	  back_urls: {
+		success: "http://localhost:3000",
+		failure: "http://localhost:3000",
+		pending: "",
+	  },
+	  auto_return: "approved",
+	};
+  
+	
+	mercadopago.preferences
+	  .create(preference)
+	  .then(function (response) {
+		res.json({
+		  id: response.body.id,
+		});
+	  })
+	  .catch(function (error) {
+		console.log(error);
+	  });
+  });
+//
+
+//Inicio del servidor Express
+app.listen(8080, () => {
+	console.log("The server is now running on Port 8080");
+});
+
+
+
 
